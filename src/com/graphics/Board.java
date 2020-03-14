2c80063e04edf02096b2e1ad2378a8df337fc4cf
mike-martinez-r7 <mike.martinez.r7@gmail.com>
2020-03-14 13:36:11 -0600

    Corregido error en repaint de frame

--- src/com/graphics/Board.java
+++ src/com/graphics/Board.java
@@ -13,16 +13,13 @@ import java.io.IOException;
 import java.lang.Math;
 
 public class Board extends JFrame implements ActionListener {
-    private int width;
-    private int height;
-    private Timer timer = new Timer(5, this);
-    private Cell[] grid;
-    private BufferedImage imgIcon = null;
-    private boolean isFirstLoad;
+    private int width;  //with of the main frame
+    private int height; //height of the main frame
+    private Cell[] grid;    //Grid that represents the values of the actual grid on the frame
+    private BufferedImage imgIcon = null;   //Image of a user icon
 
     public Board(int width, int height) {
         super();
-        this.isFirstLoad = true;
 
         this.width = width;
         this.height = height;
@@ -31,18 +28,21 @@ public class Board extends JFrame implements ActionListener {
         setSize(width, height);
         setVisible(true);
 
+        //Capture form resize event
         this.addComponentListener(new ComponentAdapter() {
             public void componentResized(ComponentEvent e) {
-                //e.getComponent().getSize().height
+            //e.getComponent().getSize().height
             }
         });
 
-        //Add button to move icon
+        //Add button foe to move icon on the cells
         JButton btnMoveUser = new JButton("Move user ");
         btnMoveUser.setBounds(0,0, 100, 30);
         btnMoveUser.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-                timer.start();
+                grid[0].posX = grid[1].posX + 10;
+
+                repaint();
             }
         });
 
@@ -64,7 +64,6 @@ public class Board extends JFrame implements ActionListener {
         BufferedImage imgDevil = null;
         BufferedImage imgAngel = null;
 
-
         try {
             imgDevil = ImageIO.read(new File("devil.png"));
             imgAngel = ImageIO.read(new File("angel.png"));
@@ -73,57 +72,52 @@ public class Board extends JFrame implements ActionListener {
             e.printStackTrace();
         }
 
-        if (isFirstLoad) {
-            for (int i = 1; i <= 10; i++) {
-                cordX = paddingFormX + boxHeight;
+        for (int i = 1; i <= 10; i++) {
+            cordX = paddingFormX + boxHeight;
 
-                for (int j = 1; j <= 10; j++) {
-                    Cell currentCell = new Cell();
-                    currentCell.posX = cordX;
-                    currentCell.posY = cordY;
+            for (int j = 1; j <= 10; j++) {
+                Cell currentCell = new Cell();
+                currentCell.posX = cordX;
+                currentCell.posY = cordY;
 
-                    g.setColor(Color.GRAY);
-                    g.fillRect(cordX, cordY, boxWidth, boxHeight);
-                    g.setColor(Color.WHITE);
-                    g.drawString(String.valueOf(numberBox), cordX + paddingBox, cordY + paddingBox);
+                g.setColor(Color.GRAY);
+                g.fillRect(cordX, cordY, boxWidth, boxHeight);
+                g.setColor(Color.WHITE);
+                g.drawString(String.valueOf(numberBox), cordX + paddingBox, cordY + paddingBox);
 
-                    if (numberBox % 8 == 0) {
-                        g.drawImage(imgDevil,
-                                cordX + paddingBox,
-                                cordY + paddingBox,
-                                boxWidth - paddingBox - 10,
-                                boxHeight - paddingBox - 10,
-                                null);
-                        currentCell.hasDevil = true;
-                    }
-
-                    if (numberBox % 7 == 0) {
-                        g.drawImage(imgAngel,
-                                cordX + paddingBox,
-                                cordY + paddingBox,
-                                boxWidth - paddingBox - 10,
-                                boxHeight - paddingBox - 10,
-                                null);
-                        currentCell.hasAngel = true;
-                    }
-
-                    this.grid[numberBox - 1] = currentCell;
-                    cordX = cordX + boxWidth + spacingBetweenBox;
-                    numberBox--;
+                if (numberBox % 8 == 0) {
+                    g.drawImage(imgDevil,
+                            cordX + paddingBox,
+                            cordY + paddingBox,
+                            boxWidth - paddingBox - 10,
+                            boxHeight - paddingBox - 10,
+                            null);
+                    currentCell.hasDevil = true;
                 }
 
-                cordY = cordY + boxHeight + spacingBetweenBox;
+                if (numberBox % 7 == 0) {
+                    g.drawImage(imgAngel,
+                            cordX + paddingBox,
+                            cordY + paddingBox,
+                            boxWidth - paddingBox - 10,
+                            boxHeight - paddingBox - 10,
+                            null);
+                    currentCell.hasAngel = true;
+                }
+
+                this.grid[numberBox - 1] = currentCell;
+                cordX = cordX + boxWidth + spacingBetweenBox;
+                numberBox--;
             }
+
+            cordY = cordY + boxHeight + spacingBetweenBox;
         }
 
-        this.isFirstLoad = false;
         g.drawImage(imgIcon, this.grid[0].posX, this.grid[0].posY, boxWidth - paddingBox - 10, boxHeight - paddingBox - 10, null);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        this.grid[0].posX = this.grid[0].posX + 10;
 
-        repaint();
     }
 }
